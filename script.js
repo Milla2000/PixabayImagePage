d8f5e0401ad26b78a962cd9e86ca5f02aeb22899
Milla2000 <millajesso2000@gmail.com>
2023-11-20 17:48:53 +0300

    intial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const imageContainer = document.getElementById('image-container');
+
+const images = imageContainer.querySelectorAll('img');
+
+images.forEach(image => {
+    const aspectRatio = image.naturalHeight / image.naturalWidth;
+    const calculatedWidth = 200;
+    const calculatedHeight = calculatedWidth * aspectRatio;
+    const rowSpan = Math.ceil(calculatedHeight / 10); 
+
+    image.classList.add('image-item');
+    image.style.width = `${calculatedWidth}px`;
+    image.style.height = `${calculatedHeight}px`;
+    image.style.gridRowEnd = `span ${rowSpan}`;
+
+});

21c822a7554d0d2292802f00dd55453c61b46684
Milla Jesso <89450003+Milla2000@users.noreply.github.com>
2023-11-24 18:23:04 +0300

    Update script.js
    updated js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const imageContainer = document.getElementById('image-container');
-
+ 
 const images = imageContainer.querySelectorAll('img');
-
+ 
 images.forEach(image => {
     const aspectRatio = image.naturalHeight / image.naturalWidth;
     const calculatedWidth = 200;
     const calculatedHeight = calculatedWidth * aspectRatio;
-    const rowSpan = Math.ceil(calculatedHeight / 10); 
-
+ 
+    // Calculate the number of rows the image should span
+    const rowSpan = Math.ceil(calculatedHeight / 10);
+ 
     image.classList.add('image-item');
     image.style.width = `${calculatedWidth}px`;
     image.style.height = `${calculatedHeight}px`;
     image.style.gridRowEnd = `span ${rowSpan}`;
-
+ 
 });
